8d18c3cfd88b8f4c2277044a421aa6ae86091832
Mark Hillard <markhillard@gmail.com>
2020-03-08 21:55:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,75 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
+// modules
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  app = express()
 
+// headers
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-var port = process.env.PORT || 4000;
+app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  // Important
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
+// root
 app.get('/', (req, res, next) => {
-  res.send('reCaptcha verification');
-});
+  res.send('Invisible reCAPTCHA Validation Server')
+})
 
+// process recaptcha
 app.post('/', (req, res) => {
-  if(
+  if (
     req.body.captcha === undefined ||
     req.body.captcha === '' ||
     req.body.captcha === null
-  ){
-    return res.json({"success": false, "msg":"Please select captcha"});
+  ) {
+    return res.json({
+      success: false,
+      msg: 'reCAPTCHA not defined'
+    })
   }
 
-  // Secret Key from Heroku Config Variable 'RECAPTCHA_SECRET'
-  const secretKey = process.env.RECAPTCHA_SECRET;
+  // heroku config variable
+  const secretKey = process.env.RECAPTCHA_SECRET
 
-  // Google's verification URL: https://developers.google.com/recaptcha/docs/verify
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+  // verification url
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
 
-  // Make Request To VerifyURL
+  // make request
   request(verifyUrl, (err, response, body) => {
-    body = JSON.parse(body);
+    body = JSON.parse(body)
 
-    // If Not Successful
-    if(body.success !== undefined && !body.success){
-      return res.json({"success": false, "msg":"Failed captcha verification from live server"});
+    // failure
+    if (body.success !== undefined && !body.success) {
+      return res.json({
+        success: false,
+        msg: 'reCAPTCHA failed'
+      })
     }
 
-    //If Successful
-    return res.json({"success": true, "msg":"Captcha passed from live server"});
-  });
-});
+    // success
+    return res.json({
+      success: true,
+      msg: 'reCAPTCHA verified'
+    })
+  })
+})
 
+// port
+let port = process.env.PORT || 4000
 
-app.listen(port, function () {
-  console.log('App listening on port ' + port);
-});
+// listen for connections
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
+})
