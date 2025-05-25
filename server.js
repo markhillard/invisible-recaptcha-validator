251764301372e2f913904d034c658514f1962cc5
Mark Hillard <markhillard@gmail.com>
2025-05-24 20:42:29 -0400

    Use axios
    This replaces request (deprecated)

--- server.js
+++ server.js
@@ -1,69 +1,78 @@
 // modules
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+import axios from "axios";
+import { json, urlencoded } from "body-parser";
+import express from "express";
+const app = express();
 
 // environment variables
-const secretKey = process.env.RECAPTCHA_SECRET
-let port = process.env.PORT || 4000
+const secretKey = process.env.RECAPTCHA_SECRET;
+const port = process.env.PORT || 4000;
 
 // headers
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-)
+    urlencoded({
+        extended: false,
+    }),
+);
 
-app.use(bodyParser.json())
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+app.use(json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    next();
+});
 
 // root
-app.get('/', (req, res) => {
-  res.sendStatus(200)
-})
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
 
 // process recaptcha
-app.post('/', (req, res) => {
-  if (req.body.captcha === undefined || req.body.captcha === '' || req.body.captcha === null) {
-    return res.json({
-      success: false,
-      msg: 'reCAPTCHA not defined'
-    })
-  }
-
-  // verification url
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
-
-  // make request
-  request(verifyUrl, (err, response, body) => {
-    body = JSON.parse(body)
-
-    // failure
-    if (body.success !== undefined && !body.success) {
-      return res.json({
-        success: false,
-        msg: 'reCAPTCHA failed'
-      })
+app.post("/", async (req, res) => {
+    if (
+        req.body.captcha === undefined ||
+        req.body.captcha === "" ||
+        req.body.captcha === null
+    ) {
+        return res.json({
+            success: false,
+            msg: "reCAPTCHA not defined",
+        });
     }
 
-    // success
-    return res.json({
-      success: true,
-      msg: 'reCAPTCHA verified'
-    })
-  })
-})
+    try {
+        // verification url
+        const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.socket.remoteAddress}`;
+        
+        // make request
+        const response = await axios.get(verifyUrl);
+        const data = response.data;
+        
+        // failure
+        if (data.success !== undefined && !data.success) {
+            return res.json({
+                success: false,
+                msg: "reCAPTCHA failed",
+            });
+        }
+        
+        // success
+        return res.json({
+            success: true,
+            msg: "reCAPTCHA verified",
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            msg: "Error verifying reCAPTCHA",
+        });
+    }
+});
 
 // listen for connections
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
